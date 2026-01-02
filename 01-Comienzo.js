79caa27c5a3e3b7f50be067d90469c5f5b0ded66
MaxiNMonzon <maxi.rp002@gmail.com>
2026-01-02 00:22:01 -0300

    Comienzo de curso

---
+++ 01-Comienzo.js
@@ -0,0 +1,17 @@
+console.log("Hola que tal")
+console.log('Hola que tal')
+
+// Esta es la forma de dejar un comentario
+
+
+/* esta forma es
+para
+dejarlo en varias 
+lineas */
+
+console.log(5)
+console.log("5")
+console.log(5*2)
+console.log(5**2) //exponente-elevado
+console.log(5+8)
+console.log(5-4)
---
+++ 02-Variables.js
@@ -0,0 +1,21 @@
+//var   ESTA FORMA DE DECLARAR VARIABLES ES LA QUE SE USABA ANTES. SE PODIA UTILIZAR SIN PONER VAR PERO APARECIA DE TODAS LAS MENERAS
+var maxMn = "el tipo es un pelotudo"
+console.log(maxMn)
+
+maxMn = "el tipo es un pelotudo MAS QUE ANTES"
+console.log(maxMn)
+
+
+//let   ES LO MISMO QUE LO ANTERIOR NADA MAS QUE ESTE ES MAS UTILIZADO AHORA
+let mMa = "el tipo es un pelotudo segunda parte"
+console.log(mMa)
+
+mMa = "el tipo es un pelotudo MAS QUE ANTES segunda parte"
+console.log(mMa)
+
+//const EL VALOR ES SIEMPRE EL MISMO NO LO PUEDO CAMBIAR. ES CONSTANTE, MAS ADELANTE VA A CAMBIAR CON ARREGLOS O LISTA PERO SU CONTENIDO DENTRO
+const MaiJi = "el tipo es un pelotudo tercera parte"
+console.log(MaiJi)
+/*
+MaiJi = "el tipo es un pelotudo MAS QUE ANTES tercera parte"
+console.log(MaiJi)   */
